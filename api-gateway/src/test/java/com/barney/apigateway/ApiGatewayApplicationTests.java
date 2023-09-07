886eee5b2e7afcb5627ea5c78d7baec3c3da59cd
barney <barney@barney.barney>
2023-09-07 14:27:29 -0500

    api gateway

---
+++ api-gateway/src/test/java/com/barney/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barney.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
